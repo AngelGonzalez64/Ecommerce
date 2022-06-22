b400569c69c6bafd28111e0b1c834e33ca744404
JorgeM8rales <69659467+JorgeM8rales@users.noreply.github.com>
2022-06-22 13:20:30 -0500

    correccion bd

--- roles/connect_db.php
+++ roles/connect_db.php
@@ -3,8 +3,7 @@
 
 		$mysqli = new MySQLi("localhost", "root","", "academ");
 		if ($mysqli -> connect_errno) {
-			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
-				. ") " . $mysqli -> mysqli_connect_error());
+			die( "Fallo la conexión a MySQL: (" . $mysqli );
 		}
 		else
 			//echo "Conexión exitossa!";
