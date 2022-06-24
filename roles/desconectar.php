76fac7157e541708e6ca07c3ac9a81ab7921a5af
Alex <angel.gonzalez.is@unipolidgo.edu.mx>
2022-06-23 21:04:00 -0500

    S_004-Ecommerce, Servicio de cerrar sesion dañada

--- roles/desconectar.php
+++ roles/desconectar.php
@@ -1,10 +1,10 @@
 <?php 
-session_start();
-if($_SESSION['user']){	
+session_star();
+if($_SESION['user']){	
 	session_destroy();
 	header("location:index.php");
 }
-else{
+elce{
 	header("location:index.php");
 }
 ?>
